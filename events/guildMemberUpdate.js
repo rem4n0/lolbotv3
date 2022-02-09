b9c596be7e073adf2c9872307cf13dc89df67029
Glitch (crawling-faint-friction) <none>
2022-02-09 17:12:50 +0000

    🌁🍓 Checkpoint ./events/guildMemberRoleAdd.js:24343700/712 ./events/guildMemberUpdate.js:24343700/2793

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,13 +1,23 @@
 const Discord = require('discord.js');
 module.exports = class{
-  async run(client, member, role) {
+  async run(client, member,oldNickname,newNickname) {
     let data = await Guild.findOne({guildID: member.guild.id})
-    const logChannel = member.guild.channels.cache.get(data.plguins.modlogs);
+    const logChannel = member.guild.channels.cache.get(data.plugins.modlogs);
     if (!logChannel) return member.channel.send({content:`ii`});
     const embed = new Discord.MessageEmbed()
     .setAuthor({ name: member.user.tag, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
     .setDescription(`**:writing_hand: ${member} roles has been updated.**`)
-    .addField("Role:", `✅ ${role.name}`)
+    .addFields(
+      
+      {
+        name:'oldNickname',
+        value:`${oldNickname}`,
+      },
+      {
+        name:'newNickname',
+        value:`${newNickname}`,}
+      
+      )
     .setTimestamp()
     .setFooter(member.guild.name, member.guild.iconURL({ dynamic: true }))
     return logChannel.send({ embeds: [embed] })
