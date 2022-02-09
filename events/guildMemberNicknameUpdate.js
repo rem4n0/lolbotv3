5397cc1f13f59ecd1b71a16e838d6b973843ac8b
Glitch (crawling-faint-friction) <none>
2022-02-09 16:32:51 +0000

    🎾🍤 Checkpoint ./events/guildMemberNicknameUpdate.js:24343700/73

--- events/guildMemberNicknameUpdate.js
+++ events/guildMemberNicknameUpdate.js
@@ -2,8 +2,12 @@ const Discord = require('discord.js');
 module.exports = class{
   async run(client, oldNickname, newNickname,member) {
     let data = await Guild.findOne({guildID: member.guild.id})
-    const logChannel = member.guild.channels.cache.get(data.plguins.modlogs);
-    if (!logChannel) return newNickname.channel.send({content:`ii`});
+    const logChannel = member.guild.channels.cache.get(data.plugins.modlogs);
+    if (!logChannel) return console.log('hhh')
+    
+    
+    
+    //member.channel.send({content:`ii`});
     const embed = new Discord.MessageEmbed()
     .setAuthor({ name: member.user.tag, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
     .setDescription(`**:writing_hand: nick name has been updated.**`)
