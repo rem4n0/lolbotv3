0f5c6d6245fa89a2130916d56381e4e2ef87bb33
Glitch (crawling-faint-friction) <none>
2022-02-09 12:04:26 +0000

    🍦👋 Checkpoint ./events/roleUpdate.js:24343700/6478

---
+++ events/roleUpdate.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+
+module.exports = class  {
+async run(bot, newRole,oldRole){
+  let data = await Guild.findOne({guildID: newRole.guild.id})
+    const logChannel = newRole.guild.channels.cache.get(data.plugins.modlogs);
+    if (!logChannel) return;
+    const allLogs = await newRole.guild.fetchAuditLogs({ type: "ROLE_UPDATE" });
+    const fetchModerator = await allLogs.entries.first();
+    if (oldRole.color !== newRole.color) {
+        const embed = new Discord.MessageEmbed()
+        .setAuthor({ name: newRole.guild.name, iconURL: newRole.guild.iconURL({ dynamic: true }) })
+        .setDescription(`😛 **\`${newRole.name}\` has been updated.**`)
+        .setFooter({ text: fetchModerator.executor.tag, iconURL: fetchModerator.executor.displayAvatarURL({ dynamic: true }) })
+        .setTimestamp()
+        .addFields(
+            {
+                name: "Old Color:",
+                value: oldRole.hexColor
+            },
+            {
+                name: "New Color:",
+                value: newRole.hexColor
+            },
+            {
+                name: "Responsible Moderator:",
+                value: `<@${fetchModerator.executor.id}>`
+            }
+        )
+        return logChannel.send({ embeds: [embed] })
+    }
+    if (oldRole.name !== newRole.name) {
+        const embed = new Discord.MessageEmbed()
+        .setAuthor({ name: newRole.guild.name, iconURL: newRole.guild.iconURL({ dynamic: true }) })
+        .setDescription(`😛 **\`${newRole.name}\` has been updated.**`)
+        .setFooter({ text: fetchModerator.executor.tag, iconURL: fetchModerator.executor.displayAvatarURL({ dynamic: true }) })
+        .setTimestamp()
+        .addFields(
+            {
+                name: "Old name:",
+                value: oldRole.name
+            },
+            {
+                name: "New name:",
+                value: newRole.name
+            },
+            {
+                name: "Responsible Moderator:",
+                value: `<@${fetchModerator.executor.id}>`
+            }
+        )
+        return logChannel.send({ embeds: [embed] })
+    }
+}}
