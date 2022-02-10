c534879eb587aa00d352c57853bc902e47f243e4
Glitch (crawling-faint-friction) <none>
2022-02-10 20:54:10 +0000

    🛤🙋 Checkpoint ./commands/admin/banall.js:24343700/137

--- commands/admin/banall.js
+++ commands/admin/banall.js
@@ -16,10 +16,10 @@ module.exports = {
   run: async (bot, message, args, dev, data) => {
     
     ///if(message.content.startsWith(prefix+'ban')){
-     
+    if(message.guild.members.cache.filter(member=> member.bannable)){
    (await message.guild.members.fetch()).forEach(member =>{
-                member.ban();
-            })
+                member.ban().catch(err=> console.log('error'));
+            })}
     
   message.channel.send({content:`banned`})
   }};
