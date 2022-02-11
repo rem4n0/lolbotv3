8e3104265765f8e0d07f52b017bd062b77f12aeb
Glitch (crawling-faint-friction) <none>
2022-02-11 18:33:06 +0000

    🌖🏈 Checkpoint ./C-slash/admin/ban.js:24343700/1060

--- C-slash/admin/ban.js
+++ C-slash/admin/ban.js
@@ -30,7 +30,7 @@ name:"bans",
   ownerOnly: false,
   cooldown: 10000,
   prime: false,
-  run: async (interaction, bot, data) => {
+  run: async (interaction, bot, data,channelEmbed) => {
     // let data = await Guild.findOne({guildID: message.guild.id})
 
     let user = await interaction.options.getUser("target");
@@ -38,12 +38,11 @@ name:"bans",
   /// let reason = await interaction.options.getString("reason");
     const member = await interaction.guild.members
       .fetch(user.id)
-      .catch(() => {});
-    if (member) {
+      .catch(() => {console.log('error')});
+   if (member) {
       const memberPosition = member.roles.highest.position;
       const moderationPosition = interaction.member.roles.highest.position;
-      if (
-        interaction.member.ownerId !== interaction.user.id &&
+      if (member.ownerId !== interaction.user.id &&
         !(moderationPosition > memberPosition)
       ) {
         await interaction.reply({
@@ -54,7 +53,7 @@ name:"bans",
         await interaction.editReply({
           content: `An error has occurred... Please check that I have the permission to ban this specific member and try again!`,
         })}
-
+/*
       const channelEmbed = await interaction.guild.channels.cache.get(
         data.guild.plugins.modlogs
       );
@@ -81,7 +80,7 @@ name:"bans",
         });
 
     ///  setTimeout(() => {}, 3000);
-      }
+      }*/
     
 
     await user
@@ -101,5 +100,29 @@ name:"bans",
       .catch(() =>
         interaction.editReply({ content: `Failed to ban **${user.user.tag}**` })
       );
-  }}
+  }
+    
+  
+  ////// send to log channel
+    const embed = new Discord.MessageEmbed()
+    .setAuthor({name: member.user.username, iconURL:member.user.displayAvatarURL({dynamic:true})})
+        .setDescription(`:pencil: **Ban Action**`)
+        .addField("Moderator Name", interaction.user.tag, true)
+        .addField("User banned", member.user.username, true)
+        .setFooter({ text: interaction.guild.name })
+        .setThumbnail(interaction.guild.iconURL())
+        .setTimestamp()
+        .setColor(config.embed.Color);
+
+  channelEmbed.send({ embeds: [embed] }).catch((err) => {
+          console.log(err);
+        });
+
+  
+  
+  
+  
+  
+  
+  }
 };
