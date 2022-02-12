7b22712a672436ae4544509e8a2d25c8e393238a
Glitch (crawling-faint-friction) <none>
2022-02-12 17:00:19 +0000

    👄🐌 Checkpoint ./events/voiceStateUpdate.js:24343700/33

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -14,13 +14,13 @@ module.exports= class{
   // Add serverdata to profile if it doesn't exist yet
   // -1 means the index couldn't be found
   if (index === -1){
-    res.data.xp.push({
+    res.data.voice.push({
       id: newStats.guild.id,
       voice: 0,
       level: 1
     });
-    res.save();
-    index = res.data.xp.findIndex(x => x.id === newStats.guild.id);
+   res.save();
+    index = res.data.voice.findIndex(x => x.id === newStats.guild.id);
     [ serverdata ] = res.data.voice.splice(index,1);
   } else {
     [ serverdata ] = res.data.voice.splice(index,1);
