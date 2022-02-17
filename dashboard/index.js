f5a71d63e1932f94e04569403db6aeb71b156f99
Glitch (crawling-faint-friction) <none>
2022-02-17 16:12:49 +0000

    🌞🐵 Checkpoint

---
+++ dashboard/index.js
@@ -0,0 +1,50 @@
+
+
+const express = require ('express');
+global.app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}))
+
+const ejs = require ('ejs');
+global.mongoose = require("mongoose");
+
+const path = require ("path");
+
+
+
+
+
+
+    app.set('views', path.join(__dirname, './src/views'));
+
+    app.engine("html", ejs.renderFile);
+    app.set("view engine", "ejs");
+  
+
+
+
+app.use("/", require ('./src/routes/index.js'));
+
+app.use(express.static(path.join(__dirname, "src/css")))
+		
+const http = require('http').createServer(app);
+   http.listen(80, () => { console.log("Website running on 80 port.")});
+
+  app.listen(8080,async()=>{
+  
+  console.log('data was redy')})
+
+  app.use((req, res) => {
+        req.query.code = 404;
+        req.query.message = `Page not found.`;
+    
+        res.status(404).render("error.ejs", {
+            
+        })
+    
+
+
+      
+  
+        
+    });
