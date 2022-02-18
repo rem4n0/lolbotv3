2dbfd356d3df212c491b6d4ee86633d0d90ac1c5
Glitch (crawling-faint-friction) <none>
2022-02-18 07:09:04 +0000

    🦄💧 Checkpoint ./dashboard/routes/index.js:24343700/43 ./dashboard/index.js:24343700/18853

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,119 +1,118 @@
+const express = require("express");
+const app = (global.app = express());
+const bodyParser = require("body-parser");
 
+const ejs = require("ejs");
 
-const express = require ('express');
-const app =(global.app = express());
-const bodyParser = require('body-parser');
+const url = require("url");
+const path = require("path");
 
+const passport = require("passport");
+const session = require("express-session");
+const Strategy = require("passport-discord").Strategy;
 
-const ejs = require ('ejs');
+const Discord = require("discord.js");
+//const channels = config.server.channels;
 
+const MemoryStore = require("memorystore")(session);
+const fetch = require("node-fetch");
+const cookieParser = require("cookie-parser");
+const referrerPolicy = require("referrer-policy");
+app.use(referrerPolicy({ policy: "strict-origin" }));
+const rateLimit = require("express-rate-limit");
+var MongoStore = require("rate-limit-mongo");
 
-  const url = require("url");
-  const path = require("path");
-  
-  const passport = require("passport");
-  const session = require("express-session");
-  const Strategy = require("passport-discord").Strategy;
-  
-  
-  const Discord = require("discord.js");
-  //const channels = config.server.channels;
-  
-  const MemoryStore = require("memorystore")(session);
-  const fetch = require("node-fetch");
-  const cookieParser = require('cookie-parser');
-  const referrerPolicy = require('referrer-policy');
-  app.use(referrerPolicy({ policy: "strict-origin" }))
-  const rateLimit = require("express-rate-limit");
-  var MongoStore = require('rate-limit-mongo');
+module.exports = async (bot) => {
+  app.set("views", path.join(__dirname, "./views"));
 
+  app.engine("html", ejs.renderFile);
+  app.set("view engine", "ejs");
 
+  app.use("/", require("./routes/index.js"));
 
+  app.use(express.static(path.join(__dirname, "./public")));
 
-module.exports=async(bot)=>{
-
-
-
-    app.set('views', path.join(__dirname, './views'));
-
-    app.engine("html", ejs.renderFile);
-    app.set("view engine", "ejs");
-  
-
-
-
-app.use("/", require ('./routes/index.js'));
-
-app.use(express.static(path.join(__dirname, "./public")))
-		
-const http = require('http').createServer(app);
-   http.listen(8080, () => { console.log("Website running on 80 port.")});
+  const http = require("http").createServer(app);
+  http.listen(8080, () => {
+    console.log("Website running on 80 port.");
+  });
 
   /*app.listen(8080,async()=>{
   
   console.log('data was redy')})*/
 
-  
-  
   const apiLimiter = rateLimit({
-      store: new MongoStore({
-         uri: config.mongoURL,
-         collectionName: "rate-limit",
-         expireTimeMs:  60 * 60 * 1000,
-         resetExpireDateOnChange: true
-         }),
-           windowMs: 60 * 60 * 1000,
-           max: 4,
-           message:
-       ({ error: true, message:  "Too many requests, you have been rate limited. Please try again in one hour." })
-    });
+    store: new MongoStore({
+      uri: config.mongoURL,
+      collectionName: "rate-limit",
+      expireTimeMs: 60 * 60 * 1000,
+      resetExpireDateOnChange: true,
+    }),
+    windowMs: 60 * 60 * 1000,
+    max: 4,
+    message: {
+      error: true,
+      message:
+        "Too many requests, you have been rate limited. Please try again in one hour.",
+    },
+  });
 
-    var minifyHTML = require('express-minify-html-terser');
-    app.use(minifyHTML({
-        override:      true,
-        exception_url: false,
-        htmlMinifier: {
-            removeComments:            true,
-            collapseWhitespace:        true,
-            collapseBooleanAttributes: true,
-            removeAttributeQuotes:     true,
-            removeEmptyAttributes:     true,
-            minifyJS:                  true
-        }
-    }));
+  var minifyHTML = require("express-minify-html-terser");
+  app.use(
+    minifyHTML({
+      override: true,
+      exception_url: false,
+      htmlMinifier: {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes: true,
+        removeEmptyAttributes: true,
+        minifyJS: true,
+      },
+    })
+  );
 
   /*
     const templateDir = path.resolve(`${process.cwd()}${path.sep}/views`);
     app.use("/css", express.static(path.resolve(`${templateDir}${path.sep}public/css`)));
     app.use("/js", express.static(path.resolve(`${templateDir}${path.sep}public/js`)));
     app.use("/img", express.static(path.resolve(`${templateDir}${path.sep}assets/img`)));
-  */app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-      extended: true
-    }));
-    passport.serializeUser((user, done) => done(null, user));
-    passport.deserializeUser((obj, done) => done(null, obj));
-  
-    passport.use(new Strategy({
-      clientID: config.clientID,
-      clientSecret: config.secret,
-      callbackURL: config.callback,      
-      scope: ["identify", "guilds", "guilds.join"]
-    },
-    (accessToken, refreshToken, profile, done) => { 
-      process.nextTick(() => done(null, profile));
-    }));
-  
-    app.use(session({
+  */ app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((obj, done) => done(null, obj));
+
+  passport.use(
+    new Strategy(
+      {
+        clientID: config.clientID,
+        clientSecret: config.secret,
+        callbackURL: config.callback,
+        scope: ["identify", "guilds", "guilds.join"],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        process.nextTick(() => done(null, profile));
+      }
+    )
+  );
+
+  app.use(
+    session({
       store: new MemoryStore({ checkPeriod: 86400000 }),
-      secret: "#@%#&^$^$%@$^$&%#$%@#$%$^%&$%^#$%@#$%#E%#%@$FEErfgr3g#%GT%536c53cc6%5%tv%4y4hrgrggrgrgf4n",
+      secret:
+        "#@%#&^$^$%@$^$&%#$%@#$%$^%&$%^#$%@#$%#E%#%@$FEErfgr3g#%GT%536c53cc6%5%tv%4y4hrgrggrgrgf4n",
       resave: false,
       saveUninitialized: false,
-    }));
-  
-    app.use(passport.initialize());
-    app.use(passport.session());
-  
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   /*
   
@@ -123,9 +122,11 @@ const http = require('http').createServer(app);
       res.redirect("/login");
   }*/
 
-   app.get("/login", (req, res, next) => {
+  app.get(
+    "/login",
+    (req, res, next) => {
       if (req.session.backURL) {
-        req.session.backURL = req.session.backURL; 
+        req.session.backURL = req.session.backURL;
       } else if (req.headers.referer) {
         const parsed = url.parse(req.headers.referer);
         if (parsed.hostname === app.locals.domain) {
@@ -133,44 +134,39 @@ const http = require('http').createServer(app);
         }
       } else {
         req.session.backURL = "/";
-       }
+      }
       next();
     },
-    passport.authenticate("discord", { prompt: 'none' }));
-    app.get("/callback", passport.authenticate("discord", { failureRedirect: "/error?code=999&message=We encountered an error while connecting." }), async (req, res) => {
-    
-        res.redirect(req.session.backURL || '/')
-      
-        
-      
-        })
-    app.get("/logout", function (req, res) {
-      req.session.destroy(() => {
-        req.logout();
-        res.redirect("/");
-      });
+    passport.authenticate("discord", { prompt: "none" })
+  );
+  app.get(
+    "/callback",
+    passport.authenticate("discord", {
+      failureRedirect:
+        "/error?code=999&message=We encountered an error while connecting.",
+    }),
+    async (req, res) => {
+      res.redirect(req.session.backURL || "/");
+    }
+  );
+  app.get("/logout", function (req, res) {
+    req.session.destroy(() => {
+      req.logout();
+      res.redirect("/");
     });
-  
+  });
 
-  
-  
-  
-  
   app.use((req, res) => {
-        req.query.code = 404;
-        req.query.message = `Page not found.`;
-    
-        res.status(404).render("error.ejs", {
-            
-        })
-    
-global.checkAuth = (req, res, next) => {
-      if (req.isAuthenticated()) return next();
+    req.query.code = 404;
+    req.query.message = `Page not found.`;
+
+    res.status(404).render("error.ejs", {});
+
+    global.checkAuth = (req, res, next) => {
+      if (req.isAuthenticated())////
+        return next();
       req.session.backURL = req.url;
       res.redirect("/login");
-    }
-
-      
-  
-        
-    });}
+    };
+  });
+};
--- dashboard/routes/index.js
+++ dashboard/routes/index.js
@@ -1,7 +1,7 @@
 const app = require("express").Router();
 const path = require("path");
 
-app.get("/",async(req, res,next)=> {
+app.get("/",(req, res,next)=> {
   //let data = await Guild.findOne({guildID:req.
   res.render("index.ejs", {
     bot: bot,
