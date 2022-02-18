1d20b553e8f52886e1e494621bbb265955bb01c4
Glitch (crawling-faint-friction) <none>
2022-02-18 21:16:42 +0000

    🚛🍡 Checkpoint ./dashboard/routes/dashboard.js:24343700/280 ./dashboard/views/dashboard.ejs:24343700/13977 ./dashboard/public/css/style.css:24343700/130

--- dashboard/routes/dashboard.js
+++ dashboard/routes/dashboard.js
@@ -1,13 +1,13 @@
 const app = require("express").Router();
 const path = require("path");
 
-app.get("/dashboard/:userid",async(req, res,next)=> {
-  //const user = bot.users.fetch(req.user.user)
-  let data = await User.findOne({userID:req.params.userid})
+app.get("/dashboard",async(req, res,next)=> {
+  //onst user = bot.users.fetch(req.user.user)
+
   res.render("dashboard.ejs", {
     config: config,
     support:config.support,
-    data:data,
+
     bot: bot,
     user:req.isAuthenticated() ? req.user : null,
   });
