41296ca98116db678cf6850a87083da8fc847bd2
Glitch (crawling-faint-friction) <none>
2022-02-18 20:23:37 +0000

    ⛽️👴 Checkpoint ./dashboard/views/dashboard.ejs:24343700/177 ./dashboard/routes/dashboard.js:24343700/114

--- dashboard/routes/dashboard.js
+++ dashboard/routes/dashboard.js
@@ -1,12 +1,13 @@
 const app = require("express").Router();
 const path = require("path");
 
-app.get("/dashboard",(req, res,next)=> {
-  //let data = await Guild.findOne({guildID:req.
+app.get("/dashboard",async(req, res,next)=> {
+  const user = bot.users.fetch(req.user.id)
+  let data = await User.findOne({userID:user.id})
   res.render("dashboard.ejs", {
     config: config,
     support:config.support,
-    
+    data:data,
     bot: bot,
     user:req.isAuthenticated() ? req.user : null,
   });
