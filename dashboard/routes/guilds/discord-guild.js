70de7137aed1c1592dda8628805fe21be6af7b93
Glitch (crawling-faint-friction) <none>
2022-02-28 08:55:10 +0000

    🌸😘 Checkpoint ./dashboard/routes/guilds/discord-guild.js:24343700/197 ./dashboard/views/main/guilds.ejs:24343700/3428

--- dashboard/routes/guilds/discord-guild.js
+++ dashboard/routes/guilds/discord-guild.js
@@ -1,10 +1,6 @@
 const app = require('express').Router();
-const { Permissions } = require('discord.js');
+const Permissions  = require('discord.js');
 
-const flags = [
-
-	Permissions.FLAGS.MANAGE_GUILD,
-];
 
 ///const channels = global.config.server.channels;
 const Discord = require("discord.js")
@@ -15,20 +11,20 @@ console.log(" guild router loaded.");
 app.get("/dashboard/guilds", global.checkAuth, async (req,res) => {
   
   
-  
+  /*
   
   req.user.guilds.forEach((guild)=>{
     
     const perm = new Permissions(guild.permissoins)
     
        if(!perm.has('MANAGE_GUILD')){
-         
-        res.redirect("/")
+        
+        return
        }
                           
                           
                           
-                          })
+                          })*/
   
   
   
@@ -39,11 +35,8 @@ app.get("/dashboard/guilds", global.checkAuth, async (req,res) => {
         config: global.config,
         user: req.isAuthenticated() ? req.user : null,
         req: req,
-
-      flag:flags,
-      perms: Permissions,
-    
-       guildID: req.params.guildID,
+        perms: Discord.Permissions,
+        guildID: req.params.guildID,
 
 	})
   
