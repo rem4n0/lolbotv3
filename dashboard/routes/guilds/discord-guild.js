c565f1096dfb001e32f6caf2ecb4970826910267
Glitch (crawling-faint-friction) <none>
2022-02-28 08:43:12 +0000

    🏫🎒 Checkpoint ./dashboard/views/main/guilds.ejs:24343700/3750 ./dashboard/routes/guilds/discord-guild.js:24343700/2768

--- dashboard/routes/guilds/discord-guild.js
+++ dashboard/routes/guilds/discord-guild.js
@@ -15,6 +15,24 @@ console.log(" guild router loaded.");
 app.get("/dashboard/guilds", global.checkAuth, async (req,res) => {
   
   
+  
+  
+  req.user.guilds.forEach((guild)=>{
+    
+    const perm = new Permissions(guild.permissoins)
+    
+       if(!perm.has('MANAGE_GUILD')){
+         
+        res.redirect("/")
+       }
+                          
+                          
+                          
+                          })
+  
+  
+  
+  
     res.render("main/guilds.ejs", {
         bot: bot,
         path: req.path,
@@ -28,67 +46,7 @@ app.get("/dashboard/guilds", global.checkAuth, async (req,res) => {
        guildID: req.params.guildID,
 
 	})
-})/*
-app.post("/dashboard/guilds", global.checkAuth, async (req,res) => {
-    let { guildID } = req.body;
-    const guild = bot.guilds.cache.get(req.body.guildID);
-    let checkGuild = await Guild.findOne({ guildID: req.body.guildID });
   
-  //  if() return res.send({ error: true, message: "Fill the must any blanks."});
-    ///if(!link && !autoCreate) return res.send({ error: true, message: "Fill the must any blanks."});
-    if (!guild) return res.send({ error: true, message: "You have to add me to that server first." });
-    const member = guild.members.cache.get(req.user.id);
-    if(!member){
-      try{ await guild.members.fetch();
-        member = guild.members.cache.get(req.user.id);
-      } catch (err) { 
-      	res.send({ error: true, message: `Couldn't fetch the members of ${guild.id}: ${err}`})
-      }
-    }
-    
-    if (!member.permissions.has("MANAGE_GUILD")) return res.send({ error: true, message: "You can only add servers with MANAGE_GUILD authorization." });
-    await Guild.updateOne({
-            guildID: req.body.guildID,
-            
-    }, {
-        $set:
-            {
-                
-                icon: guild.iconURL({ dynamic: true }),
-                ownerID: guild.owner.id ? guild.owner.id : req.user.id,
-            
-                prefix: req.body.prefix,
-              
-            }
-    }, { upsert: true })
-
-    if(autoCreate === "true") {
-    guild.fetchInvites().then(async fetchinvite => {
-      fetchinvite.array().find(a => a.inviter.id === client.user.id)
-        ? fetchinvite.array().find(a => a.inviter.id === client.user.id).code
-        : await guild.channels.cache.random().createInvite({ maxAge: 0 });
-    });
-    guild.fetchInvites().then(async fetchinvite => {
-      let inviteURL = fetchinvite
-        .array()
-        .find(a => a.inviter.id === client.user.id).url;
-    await db.updateOne({
-        id: req.params.guildID
-    }, {
-        $set: {
-            link: inviteURL
-        }
-    }, { upsert: true })
-    })
-
-    } else {
-    await db.updateOne({
-        id: req.params.guildID
-    }, {
-        $set: {
-            link: req.body.link
-        }
-    }, { upsert: true })
-    }*/
+})
 
 module.exports = app;
