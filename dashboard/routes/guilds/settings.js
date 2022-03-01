2421d19b3d365a61cd0f70c6ec0db2cde9df7d39
Glitch (crawling-faint-friction) <none>
2022-03-01 21:43:27 +0000

    💘⛸ Checkpoint ./dashboard/routes/guilds/settings.js:24343700/686 ./dashboard/index.js:24343700/70

--- dashboard/routes/guilds/settings.js
+++ dashboard/routes/guilds/settings.js
@@ -5,13 +5,29 @@ app.get("/dashboard/guild/:guildID",global.checkAuth,async(req, res,next)=> {
   
   let data = await Guild.findOne({guildID: req.params.guildID})
   
-  res.render("/main/setting.ejs", {
+  res.render("./main/setting.ejs", {
     config: config,
     support:config.support,
     data:data,
-
+    req:req,
     bot: bot,
     user:req.isAuthenticated() ? req.user : null,
   })});
-}); 
+
+
+app.post("/dashboard/guild/:guildID", global.checkAuth, async (req,res) => {
+  
+      let rbody = req.body;
+let data = await Guild.findOneAndUpdate({guildID: req.params.guildID})
+if(data){
+  
+  data.prefix= rbody["prefix"];
+  data.xp.onoff= rbody["xp"];
+  data.channels.xp= rbody["xpchannel"]
+  
+}
+data.save();
+
+  
+})
 module.exports = app;
