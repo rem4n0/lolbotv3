72a7a4428b2708e8a6b65c2995117559753f5b89
Glitch (crawling-faint-friction) <none>
2022-03-03 16:15:26 +0000

    👩‍👩‍👦⛪️ Checkpoint ./util/xp.js:24343700/12 ./dashboard/routes/dashboard.js:24343700/46 ./dashboard/views/dashboard.ejs:24343700/8

--- dashboard/routes/dashboard.js
+++ dashboard/routes/dashboard.js
@@ -5,17 +5,17 @@ app.get("/dashboard",global.checkAuth,async(req, res,next)=> {
   
   const user = bot.users.fetch(req.user.id).then(async(a)=>{
   let data = await User.findOne({userID:a.id});
-/*const server_rank = await User.find({})
+const user_rank = await User.find({})
                                                           
       .then(docs => Promise.resolve(docs.sort((A,B) => B.data.global_xp - A.data.global_xp)))
       .then(sorted => sorted.findIndex(x => x.userID === data.userID) + 1);
 
-  */res.render("dashboard.ejs", {
+  res.render("dashboard.ejs", {
     
     config: config,
     support:config.support,
     data:data,
-
+user_rank:user_rank,
     bot: bot,
     user:req.isAuthenticated() ? req.user : null,
   })});
