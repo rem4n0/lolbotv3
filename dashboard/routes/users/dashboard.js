7dc790480defd0e6c02388201fb4893897cf551c
Glitch (crawling-faint-friction) <none>
2022-03-04 18:09:44 +0000

    🍙🔋 Checkpoint ./dashboard/routes/users/dashboard.js:24343700/30

--- dashboard/routes/users/dashboard.js
+++ dashboard/routes/users/dashboard.js
@@ -4,7 +4,9 @@ const Discord = require ("discord.js")
 app.get("/dashboard",global.checkAuth,async(req, res,next)=> {
   
   const user = bot.users.fetch(req.user.id).then(async(a)=>{
+  
   let data = await User.findOne({userID:a.id});
+    if(!data){ r
 const user_rank = await User.find({})
   .then(docs => Promise.resolve(docs.sort((A,B) => B.data.global_xp - A.data.global_xp)))
   .then(sorted => sorted.findIndex(x => x.userID === data.userID) + 1);
