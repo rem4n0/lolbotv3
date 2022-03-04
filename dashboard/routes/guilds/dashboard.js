037d726df1635b7350ec3004a8c92b62dc1fccf4
Glitch (crawling-faint-friction) <none>
2022-03-04 15:38:14 +0000

    🐘🚁 Checkpoint ./dashboard/routes/users/dashboard.js:24343700/131 ./dashboard/views/users/dashboard.ejs:24343700/2589 ./dashboard/views/guild/dashboard.ejs:24343700/1279 ./dashboard/routes/guilds/dashboard.js:24343700/632

--- dashboard/routes/guilds/dashboard.js
+++ dashboard/routes/guilds/dashboard.js
@@ -3,11 +3,47 @@ const path = require("path");
 const Discord = require ("discord.js")
 app.get("/dashboard/guild/:guildID",global.checkAuth,async(req, res,next)=> {
   
-  const user = bot.guilds.fetch(req.params.guildID).then(async(a)=>{
+  const guild = bot.guilds.fetch(req.params.guildID).then(async(a)=>{
   let data = await Guild.findOne({guildID:a.id});
   
+    
+    
+    
+    const join1 = []
+      const leave1 = []
+      const join2 = []
+      const leave2 = []
+
+    
+guild.members.cache.forEach(async(user)=>{
+
+    let day = 7 * 86400000;
+    let x = Date.now() - user.joinedAt;
+    let created = Math.floor(x / 86400000);
+    
+   
+
+    if(7 > created) {
+    join2.push(user.id)
+    }
+       if(1 > created) {
+    join1.push(user.id)
+    }})
+
+     
+
+    
+    
+    
   res.render("./guild/dashboard.ejs", {
     config: config,
+    
+    join1:join1.length || 0,
+        join2: join2.length || 0,
+        leave1: leave1.length || 0,
+       // nickname: guild.me.nickname || guild.me.user.username,
+        leave2: leave2.length || 0,
+
     support:config.support,
     data:data,
     res:res,
--- dashboard/routes/users/dashboard.js
+++ dashboard/routes/users/dashboard.js
@@ -8,18 +8,17 @@ app.get("/dashboard",global.checkAuth,async(req, res,next)=> {
 const user_rank = await User.find({})
   .then(docs => Promise.resolve(docs.sort((A,B) => B.data.global_xp - A.data.global_xp)))
   .then(sorted => sorted.findIndex(x => x.userID === data.userID) + 1);
-    if(!user){ res.redirect(`/error?code=404&message= i can't fetch
+    if(!user){ res.redirect(`/error?code=404&message= I can't fetch you, yu must be a user of bot then try again`)}
   res.render("./users/dashboard.ejs", {
     
     config: config,
     support:config.support,
     data:data,
 user_rank:user_rank,
-    //flag:flags,
+    
       res:res,
-        perms: Discord.Permissions,
-        guildID: req.params.guildID,
-
+  perms: Discord.Permissions,
+   guildID: req.params.guildID,
     bot: bot,
     user:req.isAuthenticated() ? req.user : null,
   })});
