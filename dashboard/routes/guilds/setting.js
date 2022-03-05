d948ebd68ecbe2b6f36ad1cd743e7a0a57bb2101
Glitch (crawling-faint-friction) <none>
2022-03-05 20:44:40 +0000

    💴⛄️ Checkpoint ./dashboard/routes/guilds/setting.js:24343700/241

--- dashboard/routes/guilds/setting.js
+++ dashboard/routes/guilds/setting.js
@@ -5,33 +5,34 @@ app.get(
   "/dashboard/guild/:guildID/setting",
   global.checkAuth,
   async (req, res, next) => {
-    const guild = bot.guilds.fetch(req.params.guildID).then(async (a) => {
-      let data = await Guild.findOne({ guildID: a.id });
+    const guild = bot.guilds.cache.get(req.params.guildID)
+      let data = await Guild.findOne({ guildID: guild.id });
 
       res.render("./guild/setting.ejs", {
         config: config,
         data: data,
         req: req,
         bot: bot,
-        guild: a,
+        guild: guild,
         user: req.isAuthenticated() ? req.user : null,
-      });
-    });})
+      })
+    });
 
     app.post(
       "/dashboard/guild/:guildID/setting",
       global.checkAuth,
       async (req, res) => {
-        const guild = bot.guilds.cache.get(
+        const guild = bot.guilds.cache.get(req.params.guildID);
         let rbody = req.body;
-        let data = await Guild.findOne({ guildID: a.id });
+      
        if (rbody["prefix"].length > 5){
           return res.redirect( 
             "?error=true&message=You cant add up 5 words to prefix..");}
         
         
         await Guild.findOneAndUpdate({
-          guildID: a.id},{
+          guildID: req.params.guildID
+        },{
           $set:{
             
             prefix:rbody["prefix"],
@@ -55,5 +56,5 @@ app.get(
           `?success=true&message=Your changes have been successfully applied`
         );
       }
-    );})
+    );
 module.exports = app;
