ec7824d3314e25ea443a770caeccfb7274b38fec
Glitch (crawling-faint-friction) <none>
2022-03-07 20:57:45 +0000

    😄🌴 Checkpoint ./dashboard/routes/guilds/xpsystem.js:24343700/6984 ./dashboard/views/guild/xpsystem.ejs:24343700/49

--- dashboard/routes/guilds/xpsystem.js
+++ dashboard/routes/guilds/xpsystem.js
@@ -5,55 +5,58 @@ app.get(
   "/dashboard/guild/:guildID/xpsystem",
   global.checkAuth,
   async (req, res, next) => {
-    const guild = bot.guilds.cache.get(req.params.guildID)
-  let data = await Guild.findOne({guildID: guild.id});
-      res.render("./guild/xpsystem.ejs", {
-        config: config,
-        data:data,
-        req: req,
-        bot: bot,
-        guild: guild,
-        user: req.isAuthenticated() ? req.user : null,
-      })
+    const guild = bot.guilds.cache.get(req.params.guildID);
+    let data = await Guild.findOne({ guildID: guild.id });
+    res.render("./guild/xpsystem.ejs", {
+      config: config,
+      data: data,
+      req: req,
+      bot: bot,
+      guild: guild,
+      user: req.isAuthenticated() ? req.user : null,
     });
+  }
+);
 
-    app.post(
-      "/dashboard/guild/:guildID/xpsystem",
-      global.checkAuth,
-      async (req, res) => {
-        const guild = bot.guilds.cache.get(req.params.guildID);
-        let { xpchannel , xpmax, xpmin, xpmessage}= req.body;
-        
-      
-     if (xpmax.length > 10){
-          return res.redirect( 
-            "?error=true&message=You cant add up 10 point of maximum xp..");}
-if(req.body.min.length > 5){ return res.redirect("?error=true&message= you can't add up 5 point of minimum xp")}
-        let data = await Guild.findOne({guildID: guild.id})
-        
-        
-       
-        
-        await Guild.findOneAndUpdate({
-          
-          guildID: req.params.guildID},{
-          $set:{
-  
-            "channels.xp":xpchannel||null,
-            "xp.max":xpmax,
-            "xp.min": xpmin,    
-            
-            "xp.message": xpmessage,
-            
-          }})
-                
-      
-        
-   /*   
-      res.redirect(
-          `?success=true&message= applied`
-        );*/
-      return res.send({ success: true, message: "successfully" });
+app.post(
+  "/dashboard/guild/:guildID/xpsystem",
+  global.checkAuth,
+  async (req, res) => {
+    const guild = bot.guilds.cache.get(req.params.guildID);
+    let { xpchannel, xpmax, xpmin, xpmessage } = req.body;
+
+    if (xpmessage.length > 200) {
+      res.send({ error: true, message: " level message too long" });
+    }
+    if (xpmax > 10) {
+      return res.send({
+        error: true,
+        message: " maximum of xp point should less than 10",
+      });
+    }
+    if (xpmin > 5) {
+      return res.send({
+        error: true,
+        message: " minimum xp points should less than 5",
+      });
+    }
+    let data = await Guild.findOne({ guildID: guild.id });
+
+    await Guild.findOneAndUpdate(
+      {
+        guildID: req.params.guildID,
+      },
+      {
+        $set: {
+          "channels.xp": xpchannel || null,
+          "xp.max": xpmax,
+          "xp.min": xpmin,
+          "xp.message": xpmessage,
+        },
       }
     );
+
+    return res.send({ success: true, message: "successfully" });
+  }
+);
 module.exports = app;
