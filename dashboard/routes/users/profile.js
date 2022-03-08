21b77ca6c426b963c86c25f1acb515e065495706
Glitch (crawling-faint-friction) <none>
2022-03-08 14:39:28 +0000

    👗🍜 Checkpoint ./dashboard/views/users/profile.ejs:24343700/178 ./dashboard/routes/users/profile.js:24343700/352

--- dashboard/routes/users/profile.js
+++ dashboard/routes/users/profile.js
@@ -1,7 +1,7 @@
 const app = require("express").Router();
 const path = require("path");
 const { createCanvas, loadImage } = require("canvas");
-
+const market = require (`${process.cwd()}/shop/market.json`);
 console.log("profile router loaded");
 
 app.get(
@@ -11,17 +11,18 @@ app.get(
     
     const user = bot.users.fetch(req.params.userID).then(async (a)=>{
   let data = await User.findOne({userID: a.id});
-  let userdata = await User.find({});
-    
+      data.inventory.map((chunk, i, o)=>{
+      chunk.sort((A,B) => A.id - B.id ).map(d => {
+        const item = market.find(x => x.id == d.id);  
       res.render("./users/profile.ejs", {
         config: config,
         data:data,
-      
+      items: item,
         req: req,
         bot: bot,
       userdata:userdata,
         member:a,
         user: req.isAuthenticated() ? req.user : null,
       })
-    })});
+    })});})})
 module.exports = app;
