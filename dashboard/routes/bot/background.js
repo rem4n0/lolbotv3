d7f1268182521412e4e9a428da9e8436e3df9da3
Glitch (crawling-faint-friction) <none>
2022-03-13 21:00:20 +0000

    🌈🏘 Checkpoint ./dashboard/views/bot/background.ejs:24343700/26 ./dashboard/routes/guilds/logs.js:24343700/2802 ./dashboard/routes/bot/background.js:24343700/1519

---
+++ dashboard/routes/bot/background.js
@@ -0,0 +1,57 @@
+const app = require("express").Router();
+const path = require("path");
+console.log("setting router loaded");
+app.get(
+  "/dashboard/guild/:guildID/logsystem",
+  global.checkAuth,
+  async (req, res, next) => {
+    const guild = bot.guilds.cache.get(req.params.guildID);
+    let data = await Guild.findOne({ guildID: guild.id });
+    res.render("./bot/background.ejs", {
+      config: config,
+      
+      data: data,
+      req: req,
+      bot: bot,
+  
+      user: req.isAuthenticated() ? req.user : null,
+    });
+  }
+);
+
+app.post( "/bgs",
+  global.checkAuth,
+  async (req, res) => {
+    const guild = bot.guilds.cache.get(req.params.guildID);
+    let { logchannel, logon, channelCreate,channelDelete,roleCreate,roleDelete,roleUpdate } = req.body;
+
+    
+    let data = await Guild.findOne({ guildID: guild.id });
+
+    await Guild.findOneAndUpdate(
+      {
+        guildID: req.params.guildID,
+      },
+      {
+        $set: {
+          "logs.logchannel": logchannel,
+          "logs.on": logon,
+          "logs.roleCreate": roleCreate,
+          "logs.roleDelete": roleDelete,
+          "logs.roleUpdate": roleUpdate,
+          "logs.channelCreate":channelCreate,
+          "logs.channelDelete":channelDelete,
+
+        },
+      }
+    );
+
+      
+      
+      
+    
+
+    return res.send({ success: true, message: "successfully" });
+  }
+);
+module.exports = app;
