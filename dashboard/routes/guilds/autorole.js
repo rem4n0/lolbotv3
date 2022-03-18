233b8972592f963be138df6aecceb66fbd959d3c
Glitch (crawling-faint-friction) <none>
2022-03-18 19:39:28 +0000

    🙈🍫 Checkpoint ./dashboard/views/guild/setting.ejs:24343700/6908 ./dashboard/views/guild/admin.ejs:24343700/3742 ./dashboard/routes/guilds/setting.js:24343700/2898 ./dashboard/routes/guilds/autorole.js:24343700/1629

---
+++ dashboard/routes/guilds/autorole.js
@@ -0,0 +1,54 @@
+const app = require("express").Router();
+const path = require("path");
+console.log("autorole router loaded");
+app.get(
+  "/dashboard/guild/:guildID/autorole",
+  global.checkAuth,
+  async (req, res, next) => {
+    const guild = bot.guilds.cache.get(req.params.guildID);
+    let data = await Guild.findOne({ guildID: guild.id });
+  const user = guild.members.cache.get(req.user.id);
+    if (!user.permissions.has("MANAGE_GUILD")) {
+      res.redirect("?error=true&message= You can't access to this page");
+    }
+    res.render("./guild/autorole.ejs", {
+      config: config,
+      data: data,
+      req: req,
+      bot: bot,
+      guild: guild,
+      user: req.isAuthenticated() ? req.user : null,
+    });
+  }
+);
+
+app.post(
+  "/dashboard/guild/:guildID/autorole",
+  global.checkAuth,
+  async (req, res) => {
+    const guild = bot.guilds.cache.get(req.params.guildID);
+    let rbody = req.body;
+
+    if (rbody["prefix"].length > 5) {
+      return res.send({ error: true, message: "you can't add up 5 words" });
+    }
+
+    let data = await Guild.findOne({ guildID: guild.id });
+
+    await Guild.findOneAndUpdate(
+      {
+        guildID: req.params.guildID,
+      },
+      {
+        $set: {
+          prefix: rbody["prefix"],
+          "xp.onoff": rbody["onoff"],
+        },
+      }
+    );
+
+   return res.send({ success: true, message: "successfully" });
+  }
+);
+
+module.exports = app;
