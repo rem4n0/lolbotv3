afa788934f7d4ebbc4213d11af7ea86085153480
Glitch (crawling-faint-friction) <none>
2022-03-18 05:42:59 +0000

    🚙⛱ Checkpoint ./dashboard/routes/guilds/setting.js:24343700/3233 ./dashboard/views/guild/setting.ejs:24343700/99

--- dashboard/routes/guilds/setting.js
+++ dashboard/routes/guilds/setting.js
@@ -5,52 +5,49 @@ app.get(
   "/dashboard/guild/:guildID/setting",
   global.checkAuth,
   async (req, res, next) => {
-    const guild = bot.guilds.cache.get(req.params.guildID)
-  let data = await Guild.findOne({guildID: guild.id});
-    const user= guild.members.cache.get(req.user.id);
-  if(!user.permissions.has("MANAGE_GUILD")){ 
-    
-    
-  
-    res.send(" You can't access")
-  }
-      res.render("./guild/setting.ejs", {
-        config: config,
-        data:data,
-        req: req,
-        bot: bot,
-        guild: guild,
-        user: req.isAuthenticated() ? req.user : null,
-      })
+    const guild = bot.guilds.cache.get(req.params.guildID);
+    let data = await Guild.findOne({ guildID: guild.id });
+    /*const user = guild.members.cache.get(req.user.id);
+    if (!user.permissions.has("MANAGE_GUILD")) {
+      res.redirect("?error=true&message= You can't access to this page");
+    }*/
+    res.render("./guild/setting.ejs", {
+      config: config,
+      data: data,
+      req: req,
+      bot: bot,
+      guild: guild,
+      user: req.isAuthenticated() ? req.user : null,
     });
+  }
+);
 
-    app.post(
-      "/dashboard/guild/:guildID/setting",
-      global.checkAuth,
-      async (req, res) => {
-        const guild = bot.guilds.cache.get(req.params.guildID);
-        let rbody = req.body;
-      
-       if (rbody["prefix"].length > 5){
-          return res.send({error:true, message:"you can't add up 5 words"});}
-        let data = await Guild.findOne({guildID: guild.id})
-        
-        
-        
-        
-        await Guild.findOneAndUpdate({
-          
-          guildID: req.params.guildID},{
-          $set:{
-  
-            prefix:rbody["prefix"],
-           "xp.onoff":rbody["onoff"], 
-          }})
-                
-      
-        
-      
-      return res.send({ success:true, message:"successfully" });
+app.post(
+  "/dashboard/guild/:guildID/setting",
+  global.checkAuth,
+  async (req, res) => {
+    const guild = bot.guilds.cache.get(req.params.guildID);
+    let rbody = req.body;
+
+    if (rbody["prefix"].length > 5) {
+      return res.send({ error: true, message: "you can't add up 5 words" });
+    }
+
+    let data = await Guild.findOne({ guildID: guild.id });
+
+    await Guild.findOneAndUpdate(
+      {
+        guildID: req.params.guildID,
+      },
+      {
+        $set: {
+          prefix: rbody["prefix"],
+          "xp.onoff": rbody["onoff"],
+        },
       }
     );
+
+    return res.send({ success: true, message: "successfully" });
+  }
+);
 module.exports = app;
