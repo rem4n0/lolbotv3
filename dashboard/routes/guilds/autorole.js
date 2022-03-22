1e822cb04ef0e19fd0fcb8dabb464e2b4620fc49
Glitch (crawling-faint-friction) <none>
2022-03-22 08:57:13 +0000

    👨‍❤️‍👨💝 Checkpoint ./dashboard/views/guild/autorole.ejs:24343700/80 ./dashboard/routes/guilds/autorole.js:24343700/69

--- dashboard/routes/guilds/autorole.js
+++ dashboard/routes/guilds/autorole.js
@@ -27,7 +27,7 @@ app.post(
   global.checkAuth,
   async (req, res) => {
     const guild = bot.guilds.cache.get(req.params.guildID);
-    let rbody = req.body;
+    let { onoff, role} = req.body;
 
     let data = await Guild.findOne({ guildID: guild.id });
 
@@ -35,8 +35,8 @@ app.post(
       { guildID: req.params.guildID },
       {
         $set: {
-          "plugins.autorole.enabled": Boolean(rbody["onoff"]) ||true,
-          "plugins.autorole.role": rbody["role"],
+          "plugins.autorole.enabled": Boolean(onoff),
+          "plugins.autorole.role": role
         },
       }
     );
