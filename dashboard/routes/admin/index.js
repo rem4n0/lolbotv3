15e318cae5658fb738920500b077f24e5923615b
Glitch (crawling-faint-friction) <none>
2022-03-23 23:49:27 +0000

    🗃🍥 Checkpoint ./dashboard/views/admin/administrator/user-ban.ejs:24343700/118 ./dashboard/views/admin/static/head.ejs:24343700/2760 ./dashboard/routes/admin/index.js:24343700/324 ./dashboard/views/admin/index.ejs:24343700/66

--- dashboard/routes/admin/index.js
+++ dashboard/routes/admin/index.js
@@ -3,10 +3,16 @@ const app = require('express').Router();
 
 console.log("Admin/Index router loaded.");
 app.get("/admin", global.checkAuth, async (req, res) => {
-	
+	let userdata = await User.find();
+  let guilddata = await Guild.find();
+  let mutedata = await Mute.find()
+  let partnerdata = await Partner.find();
+  let primedata = await Prime.find();
 	let siteD = await Site.findOne({ id: config.clientID });
     res.render("admin/index.ejs", {
     	bot: bot,
+      userdata:userdata,
+      guilddata:guilddata,
         path: req.path,
         config: config,
         user: req.isAuthenticated() ? req.user : null,
