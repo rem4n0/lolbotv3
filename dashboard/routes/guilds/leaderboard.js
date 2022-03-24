4f6b205a881d9b3819fceafc458a6ec3e21fbbb4
Glitch (crawling-faint-friction) <none>
2022-03-24 15:11:41 +0000

    🐫🚲 Checkpoint ./dashboard/routes/guilds/leaderboard.js:24343700/613 ./dashboard/views/guild/leaderboard.ejs:24343700/490

--- dashboard/routes/guilds/leaderboard.js
+++ dashboard/routes/guilds/leaderboard.js
@@ -4,9 +4,21 @@ const type = require ('typed.js')
 app.get("/dashboard/:guildID/leaderboard",(req, res,next)=> {
   
   const guild = bot.guilds.cache.get(req.params.guildID);
+  return User.find({ 'data.xp.id':guild.id}).exec( async (err, docs) => {
+  
+    
+    
+    
+    docs = docs.map(x => { return { id: x.userID, data: x.data.xp.find(x => x.id === guild.id)};})
+      .sort((A,B) => B.data.xp - A.data.xp) // Arrange by points, descending.
+      .filter(x => x.data.xp); // Remove document where xp is 0.
+const members = await guild.members
+      .fetch({ user: docs.slice(0,10).map(x => x.id) })
+      .catch(() => null)
+
   res.render("./guild/leaderboard.ejs", {
     config: config,
-    
+    membera: members,
     req: req,
  guild:guild,
     
@@ -14,6 +26,6 @@ app.get("/dashboard/:guildID/leaderboard",(req, res,next)=> {
     user:req.isAuthenticated() ? req.user : null,
   });
 }); 
-
+})
 
 module.exports = app;
