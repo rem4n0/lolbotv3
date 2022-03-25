ad828cf3aa1169cdcf056895af1b3734af05796e
Glitch (ambiguous-fish-pine) <none>
2022-03-25 16:35:56 +0000

    Rewound to commit 41dcfa2e650f3c3c2805a74da363c20dcfaf42d1 + 0 edits

--- dashboard/routes/users/dashboard.js
+++ dashboard/routes/users/dashboard.js
@@ -3,29 +3,34 @@ const path = require("path");
 const Discord = require("discord.js");
 app.get("/dashboard", global.checkAuth, async (req, res, next) => {
   const user = bot.users.fetch(req.user.id).then(async (a) => {
-  /*  let data =
+    let data =
       (await User.findOne({ userID: a.id })) ||
-      (await new User({ userID: a.id }));*/
+      (await new User({ userID: a.id }));
 
-  return User.find({}).exec( async (err, docs) => {
-
-    
-    docs = docs.map(x => { return {xp: x.data.global_xp };})
-      .sort((A,B) => B.data.global_xp - A.data.global_xp) // Arrange by points, descending.
-      .filter(x => x.global_xp); // Remove document where xp is 0.
-
-    
+    const user_rank = await User.find({})
+      .then((docs) =>
+        Promise.resolve(
+          docs.sort((A, B) => B.data.global_xp - A.data.global_xp)
+        )
+      )
+      .then((sorted) => sorted.findIndex((x) => x.userID === data.userID) + 1);
+    if (!user) {
+      res.redirect(
+        `/error?code=404&message= I can't fetch you, yu must be a user of bot then try again`
+      );
+    }
     res.render("./users/dashboard.ejs", {
       config: config,
       support: config.support,
-      data: docs,
-    top: top,
+      data: data,
+      user_rank: user_rank,
       req:req,
       res: res,
-  
+      perms: Discord.Permissions,
+      guildID: req.params.guildID,
       bot: bot,
       user: req.isAuthenticated() ? req.user : null,
     });
   });
-});})
+});
 module.exports = app;
