a25741a6ecaec9db712b13c71a0535c85da18116
Glitch (ambiguous-fish-pine) <none>
2022-03-29 12:55:35 +0000

    🍠⛺️ Checkpoint ./dashboard/views/guild/logsystem.ejs:24343700/8550 ./dashboard/views/guild/xpsystem.ejs:24343700/1468 ./dashboard/routes/guilds/xpsystem.js:24343700/578

--- dashboard/routes/guilds/xpsystem.js
+++ dashboard/routes/guilds/xpsystem.js
@@ -25,35 +25,35 @@ app.post(
   global.checkAuth,
   async (req, res) => {
     const guild = bot.guilds.cache.get(req.params.guildID);
-    let { xpchannel, xpmax, xpmin, xpmessage } = req.body;
+    let  rbody = req.body;
+if(Object.prototype.hasOwnProperty.call(rbody, "onoff")){
 
-    if (xpmessage.length > 200) {
+    if (rbody["message"].length > 200) {
       res.send({ error: true, message: " level message too long", });
     }
-    if (xpmax > 10) {
+    if (rbody["max"] > 10) {
       return res.send({
         error: true,
         message: " maximum of xp point should less than 10",
       });
     }
-    if (xpmin > 5) {
+    if (rbody["min"] > 5) {
       return res.send({
         error: true,
         message: " minimum xp points should less than 5",
       });
-    }
+      await Guild.findOneAndUpdate({guildID: guild.id},{$set:{ "xp.channel":rbody["channel"]
+    }}
     let data = await Guild.findOne({ guildID: guild.id });
 
     await Guild.findOneAndUpdate(
       {
-        guildID: req.params.guildID,
+        guildID: guild.id,
       },
       {
         $set: {
-          "channels.xp": xpchannel || null,
-          "xp.max": xpmax,
-          "xp.min": xpmin,
-          "xp.message": xpmessage,
+      
+          "xp.onoff":rbody["onoff"],
         },
       }
     );
