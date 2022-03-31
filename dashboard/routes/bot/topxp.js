ac54834efd54db700994f98f568d79e9c57e4417
Glitch (ambiguous-fish-pine) <none>
2022-03-31 17:05:49 +0000

    🏚🌔 Checkpoint ./dashboard/routes/bot/topxp.js:24343700/79 ./dashboard/views/bot/topxp.ejs:24343700/54 ./dashboard/views/bot/topmoney.ejs:24343700/3387 ./dashboard/views/public/css/style.css:24343700/399 ./dashboard/views/parts/head.ejs:24343700/104

--- dashboard/routes/bot/topxp.js
+++ dashboard/routes/bot/topxp.js
@@ -6,9 +6,9 @@ app.get("/top/xp", global.checkAuth, async (req, res, next) => {
     return User.find({}).exec(async (err, docs) => {
       docs = docs
         .map((x) => {
-          return { id: x.userID, data:x.data.global_xp, level:x.data.global_level };
+          return { id: x.userID, global_xp:x.data.global_xp, level:x.data.global_level };
         })
-        .sort((A, B) => B.data.global_xp - A.data.global_xp) // Arrange by points, descending.
+        .sort((A, B) => B.global_xp - A.global_xp) // Arrange by points, descending.
    // Remove document where xp is 0.
    const users = await bot.users.fetch({ user: docs.slice(0,10).map(x => x.id) })
       .catch(() => null)
