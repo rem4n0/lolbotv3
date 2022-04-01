ce124021dbe9f663eed0c6ebb205283181aa21da
Glitch (ambiguous-fish-pine) <none>
2022-04-01 16:18:51 +0000

    😼🐿 Checkpoint ./commands/general/topinvite.js:24343700/1834 ./commands/invitemanager/topinv.js:24343700/995

---
+++ commands/invitemanager/topinv.js
@@ -0,0 +1,37 @@
+  const Discord = require("discord.js");
+
+
+module.exports = {
+  name: "topinvites",
+  aliases: ["topinvite","topinv"],
+  description: "",
+  usage: [""],
+  category: [""],
+  enabled: true,
+  memberPermissions: ["SEND_MESSAGES"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  ownerOnly: false,
+  cooldown: 6000,
+  run: async (bot, message, args, dev, data) => {
+
+
+
+var invites = await message.guild.invites.fetch()
+    
+   // invites = invites.values();
+    invites, "uses", { reverse: true };
+    console.log(invites)
+    let possibleInvites = ["User Invited |  Uses "];
+    invites.forEach(i => {
+      if (i.uses === 0) {
+        return;
+      }
+      possibleInvites.push([
+        "\n " +  i.inviter.username + "  :  " + i.uses + " : " + `[LINk](https://discord.gg/my${i.code})`
+      ]);
+    });
+    let embed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .addField("Top Invites.", `${possibleInvites}`);
+
+    message.channel.send({embeds:[embed]})}}
