73b26015f011411411ef29e8e9223695d8d51423
Glitch (ambiguous-fish-pine) <none>
2022-04-03 18:37:29 +0000

    🐲🌮 Checkpoint ./dashboard/views/bot/background.ejs:24343700/571 ./dashboard/views/public/js/main.js:24343700/3

--- dashboard/views/public/js/main.js
+++ dashboard/views/public/js/main.js
@@ -1,6 +1,9 @@
 function imgError(e) {
   e.src = "https://imgur.com/tiZmKiK.png";
 };
+
+
+
     var loader = document.getElementById("preloader")
     window.addEventListener("load", function(){
       setTimeout(()=>{
