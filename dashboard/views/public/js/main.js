dd49f34850d9e49dfd15b924374477f7c91593dd
Glitch (ambiguous-fish-pine) <none>
2022-04-03 18:25:30 +0000

    🛤🏣 Checkpoint ./dashboard/views/bot/topxp.ejs:24343700/56 ./dashboard/views/parts/foot.ejs:24343700/68 ./dashboard/views/parts/meta.ejs:24343700/8808 ./dashboard/views/public/css/index.css:24343700/2337 ./dashboard/views/public/js/main.js:24343700/195

--- dashboard/views/public/js/main.js
+++ dashboard/views/public/js/main.js
@@ -1,3 +1,11 @@
 function imgError(e) {
   e.src = "https://imgur.com/tiZmKiK.png";
-}
+};
+    var loader = document.getElementById("preloader")
+    window.addEventListener("load", function(){
+      setTimeout(()=>{
+
+      loader.style.display = "none";
+        
+      },2000)
+    })
