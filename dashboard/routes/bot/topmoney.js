0093da7cb8e7970164281dbbdcd79034ac27f515
Glitch (ambiguous-fish-pine) <none>
2022-04-03 09:03:11 +0000

    🚥🔋 Checkpoint ./dashboard/views/public/css/style.css:24343700/388 ./dashboard/routes/bot/topmoney.js:24343700/18

--- dashboard/routes/bot/topmoney.js
+++ dashboard/routes/bot/topmoney.js
@@ -3,7 +3,7 @@ const path = require("path");
 const Discord = require("discord.js");
 
 function intToString (value) {
-    var suffixes = ["", "k", "m", "b","t"];
+    var suffixes = ["", "K", "M", "B","T"];
     var suffixNum = Math.floor((""+value).length/3);
     var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
     if (shortValue % 1 != 0) {
@@ -20,7 +20,7 @@ app.get("/top/money", global.checkAuth, async (req, res, next) => {
         .map((x) => {
           return { id: x.userID, money:x.money };
         })
-        .sort((A, B) => A.money - B.money) // Arrange by points, descending.
+        .sort((A, B) => B.money - A.money) // Arrange by points, descending.
    // Remove document where xp is 0.
    const users = await bot.users.fetch({ user: docs.slice(0,10).map(x => x.id) })
       .catch(() => null)
