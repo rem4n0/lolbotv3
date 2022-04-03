8e225e511e9ec4dc980f4a8d1f0767208e7ee374
Glitch (ambiguous-fish-pine) <none>
2022-04-03 08:39:11 +0000

    🌯🐦 Checkpoint ./dashboard/views/public/css/style.css:24343700/339 ./dashboard/views/bot/topmoney.ejs:24343700/7 ./dashboard/routes/bot/topmoney.js:24343700/401

--- dashboard/routes/bot/topmoney.js
+++ dashboard/routes/bot/topmoney.js
@@ -1,6 +1,18 @@
 const app = require("express").Router();
 const path = require("path");
 const Discord = require("discord.js");
+
+function intToString (value) {
+    var suffixes = ["", "k", "m", "b","t"];
+    var suffixNum = Math.floor((""+value).length/3);
+    var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
+    if (shortValue % 1 != 0) {
+        shortValue = shortValue.toFixed(1);
+    }
+    return shortValue+suffixes[suffixNum];
+}
+
+
 app.get("/top/money", global.checkAuth, async (req, res, next) => {
      
     return User.find({}).exec(async (err, docs) => {
@@ -18,6 +30,7 @@ app.get("/top/money", global.checkAuth, async (req, res, next) => {
    
    
       res.render("./bot/topmoney.ejs", {
+        convert:intToString,
         users:users,
         config: config,
         support: config.support,
