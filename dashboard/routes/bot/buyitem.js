3a49e2fab54d83cf63546aac3354a4c3948c6d3c
Glitch (ambiguous-fish-pine) <none>
2022-04-06 14:09:55 +0000

    🏪🎂 Checkpoint ./dashboard/views/bot/buyitem.ejs:24343700/84 ./dashboard/routes/bot/buyitem.js:24343700/4138

--- dashboard/routes/bot/buyitem.js
+++ dashboard/routes/bot/buyitem.js
@@ -3,68 +3,69 @@ const path = require("path");
 console.log("setting router loaded");
 const text = require(`${process.cwd()}/util/string`);
 
-const market= require(`${process.cwd()}/shop/market.json`);
-app.get("/item/:id",
-  global.checkAuth,
-  async (req, res, next) => {
-  
-const id = market.find( x => x.id == req.params.id);
+const market = require(`${process.cwd()}/shop/market.json`);
+app.get("/item/:id", global.checkAuth, async (req, res, next) => {
+  const id = market.find((x) => x.id == req.params.id);
 
-  
-    const user = bot.users.cache.get(req.user.id);
-    let data = await User.findOne({ userID: user.id});
-    res.render("./bot/buyitem.ejs", {
-      id: id,
-      config: config,
-      market: market,
-      data: data,
-      req: req,
-  img:id.assets.link,
-      bot: bot,
-  
-      user: req.isAuthenticated() ? req.user : null,
-    });
-  }
-);
+  const user = bot.users.cache.get(req.user.id);
+  let data = await User.findOne({ userID: user.id });
+  res.render("./bot/buyitem.ejs", {
+    id: id,
+    config: config,
+    market: market,
+    data: data,
+    req: req,
+    img: id.assets.link,
+    bot: bot,
 
-app.post( "/items/:id",
-  global.checkAuth,
-  async (req, res,[ amt]) => {
-  const id = market.find(x => x.id == req.params.id);
-    let rbody = req.body;
+    user: req.isAuthenticated() ? req.user : null,
+  });
+});
 
-let user = bot.users.cache.get(req.user.id);
-    let data = await User.findOne({ userID:user.id });
+app.post("/item/:id", global.checkAuth, async (req, res) => {
+  let amt;
+  const id = market.find((x) => x.id == req.params.id);
+  let rbody = req.body;
 
-   amt = Math.floor(Math.abs(amt)) || 1;
-    const total = id.price * amt;
+  let user = bot.users.cache.get(req.user.id);
+  let data = await User.findOne({ userID: user.id });
 
-if (!id.price && amt > 1){
-res.redirect ('?error=true&message="You may only have 1 free item at a time');
-}else if (data.money < total){
-res.redirect (`?error=true&message=You do not have enough credits to proceed with this transaction! You need ${text.commatize(total)} for **${amt}x ${id.name}**`)
-}
-else if (data.inventory.find(x => x.id === id.id) && !id.price){
-res.redirect (`?error=true&message=You have may only 1 free item at time`)
-}else{
-  const old = data.inventory.find(x => x.id === id.id);
-      if (old){
-        const inv = data.inventory;
-        let x = data.inventory.splice(inv.findIndex(x => x.id === old.id),1)[0];
-        x.amount = x.amount + amt;
-        data.inventory.push(data)
-      } else {
-        data.inventory.push({
-          id: id.id,
-          amount: amt
-        });
-      };
+  amt = Math.floor(Math.abs(amt)) || 1;
+  const total = id.price * amt;
 
-      data.money = data.money - total;
-      return data.save()
-  
-  
-    return res.redirect(`?success=true&message=successfully`);
-  }}
-);
+  if (!id.price && amt > 1) {
+    res.redirect(
+      '?error=true&message="You may only have 1 free item at a time'
+    );
+  } else if (data.money < total) {
+    res.redirect(
+      `?error=true&message=You do not have enough credits to proceed with this transaction! You need ${text.commatize(
+        total
+      )} for **${amt}x ${id.name}**`
+    );
+  } else if (data.inventory.find((x) => x.id === id.id) && !id.price) {
+    res.redirect(`?error=true&message=You have may only 1 free item at time`);
+  } else {
+    const old = data.inventory.find((x) => x.id === id.id);
+    if (old) {
+      const inv = data.inventory;
+      let x = data.inventory.splice(
+        inv.findIndex((x) => x.id === old.id),
+        1
+      )[0];
+      x.amount = x.amount + amt;
+      data.inventory.push(data);
+    } else {
+      data.inventory.push({
+        id: id.id,
+        amount: amt,
+      });
+    }
+
+    data.money = data.money - total;
+    return data.save().then(()=>{
+
+  res.redirect(`?success=true&message=successfully`);
+  })}
+});
 module.exports = app;
