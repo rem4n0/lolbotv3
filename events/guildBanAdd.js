7cf7357fee7bb383e99f4dd4978d25132c10e61e
Glitch (ambiguous-fish-pine) <none>
2022-04-13 16:29:48 +0000

    🎰😎 Checkpoint ./events/guildBanAdd.js:24343700/192

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,15 +1,14 @@
 const Discord = require("discord.js");
 module.exports = class {
-  async run(guild) {
-    console.log(guild);
-    let data = await Guild.findOne({ guildID: guild.id });
-    const entry1 = await guild
-      .fetchAuditLogs({ type: "GUILD_BAN_ADD" })
+  async run(ban) {
+    let data = await Guild.findOne({ guildID: ban.guild.id });
+    const entry1 = await ban.guild
+      .fetchAuditLogs({ type: "MEMBER_BAN_ADD" })
       .then((audit) => audit.entries.first());
     const user2 = entry1.executor;
-    const member = entry1.entries.first();
+   // const member = entry1.entries.first();
     try {
-      const channelEmbed = await guild.channels.cache.get(
+      const channelEmbed = await ban.guild.channels.cache.get(
         data.plugins.logs.channel
       );
 
@@ -17,10 +16,10 @@ module.exports = class {
       const embed = new Discord.MessageEmbed()
         .setDescription(`:pencil: **Ban Action**`)
         .addField("Moderator Name", user2.tag, true)
-        .addField("User banned", member.user.username, true)
+        .addField("User banned", ban.guild.user.username, true)
         .addFielf("reason", entry1.reason || "not have reason", true)
-        .setFooter({ text: guild.name })
-        .setThumbnail(guild.iconURL())
+        .setFooter({ text: ban.guild.name })
+        .setThumbnail(ban.guild.iconURL())
         .setTimestamp()
         .setColor(config.embed.Color);
 
@@ -31,7 +30,7 @@ module.exports = class {
           .permissionsFor(guild.me)
           .has(["SEND_MESSAGES", "EMBED_LINKS"])
       ) {
-        channelEmbed.send({ embeds: [embed] }).catch((err) => {
+      channelEmbed.send({ embeds: [embed] }).catch((err) => {
           console.log(err);
         });
 
