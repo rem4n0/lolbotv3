d6f89da8f4c907eeedee3bfb28cd96e9464512f5
Glitch (ambiguous-fish-pine) <none>
2022-04-21 19:05:56 +0000

    💒🏃 Checkpoint ./events/guildMemberUpdate.js:24343700/2944

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 module.exports = class{
-  async run(member,oldNickname,newNickname,oldRole,newRole) {
-    const {guild} = member
+  async run(oldMember,newMember) {
+    const {guild} = newMember
   
-  console.log(newRole)
+  console.log(newMember)
 
     let data = await Guild.findOne({guildID: guild.id})
     const logChannel = guild.channels.cache.get(data.plugins.logs.channel)
     if (!logChannel) return;// member.channel.send({content:`ii`});
     
-    
+    /*
     if(!oldNickname && newNickname){
     const embed = new Discord.MessageEmbed()
     .setAuthor({ name: member.user.tag, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
@@ -28,14 +28,14 @@ module.exports = class{
     .setTimestamp()
     .setFooter(member.guild.name, member.guild.iconURL({ dynamic: true }))
     return logChannel.send({ embeds: [embed] })
-    }
+    }*/
     const newembed = new Discord.MessageEmbed()
-    .setAuthor({name: member.guild.name, urlIcon: member.guild.iconURL({dynamic: true})})
-    if(!oldRole && newRole){
-newembed.addFields({ name: "oldRole", value:oldRole.name},
-                { name:"newRole", value:newRole.name})
+    .setAuthor({name: newMember.guild.name, urlIcon: newMember.guild.iconURL({dynamic: true})})
+  /*  if(oldMember){
+newembed.addFields({ name: "oldRole", value:},
+                { name:"newRole", value:})
       
-    }
+    }*/
     logChannel.send({embeds:[newembed]})
   
   
