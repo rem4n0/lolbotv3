e441e8159593b2bbb598108a84ecb471e7599d96
Glitch (ambiguous-fish-pine) <none>
2022-04-21 21:01:56 +0000

    🔮🏪 Checkpoint ./events/guildMemberUpdate.js:24343700/380

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,42 +1,44 @@
 const Discord = require('discord.js');
 module.exports = class{
-  async run(oldMember,newMember) {
+  async run(oldMember,newMember,old) {
     const {guild} = newMember
   
-  console.log(newMember)
+  console.log(newMember._roles)
 
     let data = await Guild.findOne({guildID: guild.id})
     const logChannel = guild.channels.cache.get(data.plugins.logs.channel)
     if (!logChannel) return;// member.channel.send({content:`ii`});
     
-    /*
-    if(!oldNickname && newNickname){
+    
+    if(!oldMember.nickname && newMember.nickname){
+      console.log("hhh")
     const embed = new Discord.MessageEmbed()
-    .setAuthor({ name: member.user.tag, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
-    .setDescription(`**:writing_hand: ${member} roles has been updated.**`)
+    .setAuthor({ name: guild.user.tag, iconURL:guild.user.displayAvatarURL({ dynamic: true }) })
+    .setDescription(`**:writing_hand: ${guild} roles has been updated.**`)
     .addFields(
       
       {
         name:'oldNickname',
-        value:`${oldNickname}`,
+        value:`${oldMember.nickname}`,
       },
       {
         name:'newNickname',
-        value:`${newNickname}`,}
+        value:`${newMember.nickname}`,}
       
       )
     .setTimestamp()
-    .setFooter(member.guild.name, member.guild.iconURL({ dynamic: true }))
+    .setFooter(newMember.guild.name,newMember.guild.iconURL({ dynamic: true }))
     return logChannel.send({ embeds: [embed] })
-    }*/
+    }
+    /*
     const newembed = new Discord.MessageEmbed()
     .setAuthor({name: newMember.guild.name, urlIcon: newMember.guild.iconURL({dynamic: true})})
   /*  if(oldMember){
 newembed.addFields({ name: "oldRole", value:},
                 { name:"newRole", value:})
       
-    }*/
-    logChannel.send({embeds:[newembed]})
+    }
+    logChannel.send({embeds:[newembed]})*/
   
   
   
