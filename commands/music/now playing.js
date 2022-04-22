76742ed6efe5b2e6bc45952ff1b1cf1b9bf5ba69
Glitch (ambiguous-fish-pine) <none>
2022-04-22 22:35:48 +0000

    ✈️👨‍❤️‍💋‍👨 Checkpoint ./commands/music/now playing.js:24343700/2032 ./commands/music/loop.js:24343700/1846

---
+++ commands/music/now playing.js
@@ -0,0 +1,54 @@
+
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const player = require("../../handler/player")
+module.exports = {
+  name: "nowplayng",
+  aliases: ["np","nowplaying"],
+  usage: ["Bonowplaying"],
+  description: "show your songs now ",
+  category: "music",
+  enabled: true,
+  ownerOnly: false,
+  cooldown: 6000,
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  memberPermissions: ["SEND_MESSAGES"],
+  run: async (client, message, args) => {
+
+           const queue = player.getQueue(message.guild.id);
+
+        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        const track = queue.current;
+
+        const embed = new MessageEmbed();
+
+        embed.setColor('RED');
+        embed.setThumbnail(track.thumbnail);
+        embed.setAuthor({name:track.title});
+
+        const methods = ['disabled', 'track', 'queue'];
+
+        const timestamp = queue.getPlayerTimestamp();
+        const trackDuration = timestamp.progress == 'Infinity' ? 'infinity (live)' : track.duration;
+
+        embed.setDescription(`Volume **${queue.volume}**%\nDuration **${trackDuration}**\nLoop mode **${methods[queue.repeatMode]}**\nRequested by ${track.requestedBy}`);
+
+        embed.setTimestamp();
+   //     embed.setFooter('Music comes first - Made with heart by Zerio ❤️', message.author.avatarURL({ dynamic: true }));
+
+        const saveButton = new MessageButton();
+
+        saveButton.setLabel('Save this track');
+        saveButton.setCustomId('saveTrack');
+        saveButton.setStyle('SUCCESS');
+
+        const row = new MessageActionRow().addComponents(saveButton);
+
+        message.channel.send({ embeds: [embed], components: [row] });
+    }, 
+    
+    
+    
+    
+    
+  }
