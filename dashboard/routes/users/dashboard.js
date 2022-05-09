e391ef526b6f6e4f7bcba017c7cac24be7ddba71
Glitch (ambiguous-fish-pine) <none>
2022-05-09 22:28:50 +0000

    👁☔️ Checkpoint ./dashboard/views/users/dashboard.ejs:24343700/5558 ./dashboard/routes/users/dashboard.js:24343700/401

--- dashboard/routes/users/dashboard.js
+++ dashboard/routes/users/dashboard.js
@@ -1,6 +1,18 @@
 const app = require("express").Router();
 const path = require("path");
 const Discord = require("discord.js");
+
+
+function intToString (value) {
+    var suffixes = ["", "K", "M", "B","T"];
+    var suffixNum = Math.floor((""+value).length/3);
+    var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
+    if (shortValue % 1 != 0) {
+        shortValue = shortValue.toFixed(1);
+    }
+    return shortValue+suffixes[suffixNum];
+}
+
 app.get("/dashboard", global.checkAuth, async (req, res, next) => {
   const maintenance = await Maintenance.findOne({
   server: config.serverid
@@ -40,6 +52,7 @@ if(maintenance && maintenance.toggle == "true") {
     }
     res.render("./users/dashboard.ejs", {
       config: config,
+      convert:intToString,
       support: config.support,
       data: data,
       user_rank: user_rank,
