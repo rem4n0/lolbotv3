58a711eff6ec2a925d8e3da95943652636dc0a67
Glitch (ambiguous-fish-pine) <none>
2022-05-10 07:53:15 +0000

    🐣🐲 Checkpoint ./C-slash/music/play.js:24343700/5097

--- C-slash/music/play.js
+++ C-slash/music/play.js
@@ -8,32 +8,44 @@ module.exports = {
   aliases: ["play"],
   usage: ["Boplay Song"],
   description: "Play your favorite music or anything you want",
-  
-options:[{
-  String:{
-    name:"song",
-    description:"find_music_by_name_and_url",
-    required:true,
-  }}],
+
+  options: [
+    {
+      String: {
+        name: "song",
+        description: "find_song",
+        required: true,
+      },
+    },
+  ],
   category: "music",
   enabled: true,
   ownerOnly: false,
   cooldown: 6000,
   botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
   memberPermissions: ["SEND_MESSAGES"],
-  run: async (client, interaction, args) => {
-
-
-    const songSearch = interaction.options.getString('input');
-    
-      
+  run: async ( interaction, args) => {
+    const songSearch =  interaction.options.getString("find_song");
+/*
     if (!songSearch)
-      return interaction.reply({content:"You didn't provide me a name of song to play!"
+      return interaction.reply({
+        content: "You didn't provide me a name of song to play!",
       });
-
-
+*/
+    
+if (interaction.member.voice.userLimit != 0 && interaction.member.voice.full)
+				return interaction.reply({
+					embeds: [new Discord.MessageEmbed()
+						.setColor(config.embed.Color)
+					
+						.setTitle(` Your Voice Channel is full, I can't join!`)
+					],
+					ephemeral: true
+				});
+    
     if (!interaction.member.voice.channel)
-      return message.reply({content:" You need to join a voice channel for me to play song!"
+      return await interaction.followUp({
+        content: " You need to join a voice channel for me to play song!",
       });
 
     const searchResult = await player.search(songSearch, {
@@ -41,33 +53,36 @@ options:[{
       searchEngine: QueryType.AUTO,
     });
 
-    
     if (!searchResult || !searchResult.tracks.length)
-      return interaction.reply({content:" Hmm, I couldn't quite find the song you requested for; try playing another one!"});
+      return interaction.reply({
+        content:
+          " Hmm, I couldn't quite find the song you requested for; try playing another one!",
+      });
 
     const queue = player.createQueue(interaction.guild, {
       metadata: interaction.channel,
-       // disabling volume controls can improve performance
-            leaveOnEnd: false,
-			leaveOnStop: false,
-            spotifyBridge: false
-
-      
+      // disabling volume controls can improve performance
+      leaveOnEnd: false,
+      leaveOnStop: false,
+      spotifyBridge: false,
     });
 
-    
     try {
-      if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+      if (!queue.connection)
+        await queue.connect(interaction.member.voice.channel);
     } catch {
       await player.deleteQueue(interaction.guild.id);
-      
-      queue.destroy();
-      return interaction.reply({content:"There was an error with your request, please trye again later!" });
-    }
-    searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
 
-    
-  
+      queue.destroy();
+      return interaction.reply({
+        content:
+          "There was an error with your request, please trye again later!",
+      });
+    }
+    searchResult.playlist
+      ? queue.addTracks(searchResult.tracks)
+      : queue.addTrack(searchResult.tracks[0]);
+
     if (!queue.playing) await queue.play();
   },
 };
