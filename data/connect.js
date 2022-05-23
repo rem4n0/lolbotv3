a59d42244c9cdf88029993f5ea1590e8609a3edb
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-05-24 02:55:57 +0300

    Update connect.js

--- data/connect.js
+++ data/connect.js
@@ -3,9 +3,9 @@ module.exports = async () => {
     mongoose.connect(config.mongoURL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    
+    useFindAndModify:false,
         autoIndex: false
     }).then(() => {
-    console.log("[vcodes.xyz]: Mongoose successfully connected.");
-    }).catch(err => console.log("[vcodes.xyz]: An error occurred while connecting mongoose.", err));
+    console.log("Mongoose successfully connected.");
+    }).catch(err => console.log(": An error occurred while connecting mongoose.", err));
 }
