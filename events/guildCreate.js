5df03ee44689dd02e152d5fd2b48c7b357fb8a15
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-05-29 21:29:49 +0300

    Update and rename evenguildCreate to events/guildCreate.js

---
+++ events/guildCreate.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const config = require(`${process.cwd()}/config.json`)
+module.exports = class {
+
+	
+	async run (guild,bot, message) {
+    const channel = await bot.channels.cache.get(config.joinchannel);
+    const owner = await guild.members.cache.get(guild.ownerId)
+    console.log(owner)
+ ////   const invite = await guild.invites.create(guild.channels.cache.firstKey())
+    const embed = new Discord.MessageEmbed()
+    .setAuthor({name: guild.name})
+    .setDescription("**Join Guild**")
+   .addFields({ name: "GUILD MEMBER COUNT", value: `${guild.memberCount}`},
+               
+               {name: "GUILD OWENR NAME", value:`${owner.user.tag}`},
+              )
+     setTimeout(async()=>{
+    await channel.send({embeds:[embed]})
+  
+     },2000)
+	}}
