b16c7d3dcc75848ad019093b5ba2e912a202805d
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-06-09 01:04:02 +0300

    Update setting.js

--- dashboard/routes/guilds/setting.js
+++ dashboard/routes/guilds/setting.js
@@ -19,8 +19,8 @@ if(maintenance && maintenance.toggle == "true") {
     
     const guild = bot.guilds.cache.get(req.params.guildID);
     let data = await Guild.findOne({ guildID: guild.id });
-    const user = guild.members.cache.get(req.user.id);
-    if (!user?.permissions?.has("MANAGE_GUILD")) {
+    const user = guild.members.fetch(req.user.id);
+      if (!user.permissions.has("MANAGE_GUILD")) {
       res.redirect("?error=true&message= You can't access to this page");
     }
     res.render("./guild/setting.ejs", {
