ecd3cade2569c979c5401e27c6ece39f9ddedce3
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-06-09 01:02:08 +0300

    Update items.js

--- dashboard/routes/bot/items.js
+++ dashboard/routes/bot/items.js
@@ -18,7 +18,7 @@ if(maintenance && maintenance.toggle == "true") {
 
 
     
-    const user = bot.users.cache.get(req.user.id);
+    //const user = bot.users.cache.get(req.user.id);
     let data = await User.findOne({ userID: user.id});
     res.render("./bot/items.ejs", {
       config: config,
@@ -37,7 +37,7 @@ app.post( "/bgs",
   async (req, res) => {
     let rbody = req.body;
 
-let user = bot.users.cache.get(req.user.id);
+     let user = bot.users.cache.get(req.user.id);
     let data = await User.findOne({ userID:user.id });
 
   
