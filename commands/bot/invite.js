82e55d8674a7e9c66dbfbf550bdcbba2bd0268b6
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-06-09 23:45:21 +0300

    Update invite.js

--- commands/bot/invite.js
+++ commands/bot/invite.js
@@ -11,16 +11,37 @@ module.exports = {
   ownerOnly: false,
   cooldown: 3000,
   run: async (bot, message, args, dev) => {
+    const row = new Discord.MessageActionRow()
+			.addComponents(
+        new Discord.MessageButton()
+					
+					.setLabel('invite')
+          .setURL('https://bobowolrd.tk/support')
+					.setStyle('LINK'),
+			);
+        
+				// ...
+			
+
+		const embed2 = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+		
+			.setURL('https://boboworld.tk/invite')
+			.setDescription('Invite bot by clicking the button');
+
+  
+    
+    /*
     let embed = new Discord.MessageEmbed()
       .setColor(config.embed.Color)
       .setTitle("**Bobo Bot**  InviteLink!")
       .setURL(
         `https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot%20applications.commands`
 )
-     
+    */ 
       
-      message.channel.send({embeds:[embed]}).catch(err=>{
-      message.author.send({embeds:[embed]})
+      message.channel.send({embeds:[embed2], components: [row]}).catch(err=>{
+      message.author.send({embeds:[embed2],components: [row]})
       })
   }
 }
