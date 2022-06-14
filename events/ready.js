8f4662c1fd9b05530c903e4b22e39df64a9390ad
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-06-14 16:03:48 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@
 module.exports= class{
   async run(bot){
 const guilds = await bot.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)
-  bot.user.setActivity(`https://boboworld.tk | ${bot.guilds.cache.size} Servers`, { type: "WATCHING" });
+  bot.user.setActivity(`Boinvite| ${bot.guilds.cache.size} Servers`, { type: "WATCHING" });
   console.log(`${bot.user.username}: registered`);
     const checkUnmutes = require("../helpers/Checkunmute.js");
 		checkUnmutes.init(bot);
