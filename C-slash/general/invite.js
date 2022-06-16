456266ef4c9d1141d52df179e8823e2c3e623e6c
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-06-16 15:29:43 +0300

    Update invite.js

--- C-slash/general/invite.js
+++ C-slash/general/invite.js
@@ -13,20 +13,15 @@ module.exports = {
   cooldown: 10000,
 prime: false,
   run: async (interaction,bot,data) => {
-const row = new Discord.MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					//.setCustomId('primary')
-					.setLabel('Link')
-.setURL('https://bobowolrd.tk/invite')
-					.setStyle('LINK'),
-			);
 
-let embed = new Discord.MessageEmbed()
+ const row = new Discord.MessageActionRow()
+.addComponents(new Discord.MessageButton()
+.setLabel('Link')
+.setURL('https://boboworld.tk/invite')
+.setStyle('LINK'),);
+
+      let embed = new Discord.MessageEmbed()
       .setColor(config.embed.Color)
       .setDescription(`Invite bot by clicking the button`)
-      
-
-      
       interaction.reply({embeds:[embed], components:[row]})
       }}
