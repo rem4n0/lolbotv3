dac19317f06b4856c627017858d92e777744d05b
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-07-18 13:20:04 +0300

    Update index.js

--- dashboard/index.js
+++ dashboard/index.js
@@ -243,9 +243,16 @@ app.use("/arc-sw.js", express.static(path.resolve(`arc-sw.js`)))
   app.use(async (req, res, next) => {
     if (req.path.includes("/admin")) {
       if (req.isAuthenticated()) {
-      
+      /*
 if(bot.guilds.cache.get(config.serverid).members.cache.get(req.user.id).roles.cache.get(global.config.server.role.administrator) || bot.guilds.cache.get(config.serverid).members.cache.get(req.user.id).roles.cache.get(global.config.server.role.moderator) || req.user.id === "768944616724103170") {
-              next();
+             */
+        let guild = await bot.guilds.cache.get(config.serverid);
+        let member = await guild.members.fetch(req.user.id);
+  if(member.roles.cache.get(config.server.role.adminstrator)|| member.roles.cache.get(config.server.role.moderator) || req.user.id === "768944616724103170"){
+     
+     
+
+ next();
                
         } else {
           res.redirect(
