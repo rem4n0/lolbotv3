96c4e150a54053a1297553e5fe1c2c76c9ed1ab5
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-07-20 20:16:10 +0300

    Update unlock.js

--- commands/moderation/unlock.js
+++ commands/moderation/unlock.js
@@ -9,7 +9,7 @@ module.exports = {
   category: ["moderation"],
   enabled: true,			  
   memberPermissions: [ "MANAGE_CHANNELS","SEND_MESSAGES" ],			
-  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","MANAGE_CHANNELS" ],		
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","MANAGE_ROLES" ],		
   ownerOnly: false,			
   cooldown: 6000,
   run: async (bot, message, args, dev, data) => {
@@ -24,29 +24,6 @@ module.exports = {
         message.channel.send({content:`channel unlocked`});
      });
     
-        /// send to log channel
-    const channelEmbed = await message.guild.channels.cache.get(data.guild.plugins.modlogs)
-
-      if(!channelEmbed) return;
-    const embed = new Discord.MessageEmbed()
-    .setDescription(`:pencil: **channel Action**`)
-    .addField('Moderator Name', message.author.toString(), true)
-    .addField('Channel',channel.name, true)
-    .setFooter({text:message.guild.name})
-    .setThumbnail(message.guild.iconURL())
-    .setTimestamp()
-    .setColor(config.embed.Color)
-  
-   
-   
-        if(channelEmbed &&
-      channelEmbed.viewable &&
-      channelEmbed.permissionsFor(message.guild.me).has(['SEND_MESSAGES', 'EMBED_LINKS'])){
-            channelEmbed.send({embeds:[embed]}).catch((err)=>{console.log(err)})
-          
-            setTimeout(()=>{
-            }, 3000)
-      }
     
     
     
