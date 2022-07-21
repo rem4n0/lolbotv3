409d9a905ef0f51e48f906dcb9711a9a02d7995f
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-07-21 17:38:11 +0300

    Update stop.js

--- C-slash/music/stop.js
+++ C-slash/music/stop.js
@@ -1,24 +1,37 @@
-
 const player = require("../../handler/player.js");
-
+const Discord = require("discord.js");
 module.exports = {
-
-name:"stop",
-  description:"stop song",
-
-  enabled: true,			    
-  memberPermissions: [ "SEND_MESSAGES" ],			
-  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],		
-  enabled:true,
-  category:["general"],
-  ownerOnly: false,			
+  name: "stop",
+  aliases: ["stop"],
+  usage: ["Bostop"],
+  description: "stop song",
+  category: "music",
+  enabled: true,
+  memberPermissions: ["SEND_MESSAGES"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  enabled: true,
+  category: ["music "],
+  ownerOnly: false,
   cooldown: 10000,
-prime: false,
-  run: async (interaction,bot,data) => {
-if(!interaction.member.voice.channel){ return interaction.reply({content:`You can't stop songs`})}
+  prime: false,
+  run: async (interaction, args) => {
     
+    
+    
+    
+    if (! interaction.member?.voice?.channel) {
+      return interaction.reply({ content: `You are not in same channel` });
+    }
+
     const queue = player.getQueue(interaction.guildId);
-    queue.stop()
-    interaction.reply({content:`Stopped 🚫`});
-    
-  }}
+    const embed = new Discord.MessageEmbed().setDescription(
+      "There is no any songs to stop"
+    );
+    if (!queue?.playing) {
+      return interaction.followUp({ embeds: [embed] });
+    }
+
+    queue.stop();
+    message.reply({ content: `Stopped 🚫` });
+  },
+};
