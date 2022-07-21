cdd93a71359767cddb9a58dab7460b095b7da747
lordm121 <74502300+lordm121@users.noreply.github.com>
2022-07-22 02:25:53 +0300

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,48 +1,63 @@
-const Discord = require('discord.js')
-
-
+const Discord = require("discord.js");
 
 module.exports = {
-    name: "ban",
-    aliases: ["band","ban"],
-    description: "You can ban a member, or multiple members using this command",
-    usage: ["ban [@User]"],
-    category: ["moderation"],
-    enabled: true,
-    memberPermissions: ["BAN_MEMBERS","SEND_MESSAGES"],
-    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS"],
-    ownerOnly: false,
-    cooldown: 6000,
-    run: async (bot, message, args, dev) => {
+  name: "ban",
+  aliases: ["band", "ban"],
+  description: "You can ban a member, or multiple members using this command",
+  usage: ["ban [@User]"],
+  category: "moderation",
+  enabled: true,
+  memberPermissions: ["BAN_MEMBERS", "SEND_MESSAGES"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS"],
+  ownerOnly: false,
+  cooldown: 6000,
+  run: async (bot, message, args, dev) => {
+  
+try{
+    let user =
+      (await message.mentions.members.first()) ||
+      (await message.guild.members.fetch(args[1]));
+
+    let reason = args.slice(2).join(" ");
+    const member = await message.guild.members.fetch(user.id).catch(() => {});
+    if (member) {
+      const memberPosition = member.roles.highest.position;
+      const moderationPosition = message.member.roles.highest.position;
+      if (
+        message.member.ownerId !== message.author.id &&
+        !(moderationPosition > memberPosition)
+      ) {
+        return message.channel.send({
+          content: `You can't sanction or update a sanction for a member who has an higher or equal role hierarchy to yours!
+    `,
+        });
+      }
+      if (!member.bannable) {
+        return message.channel.send({
+          content: `An error has occurred... Please check that I have the permission to ban this specific member and try again!`,
+        });
+      }
+
+      await user
+        .send(
+          `**${message.author.tag}** banned you from ${
+            message.guild.name
+          }!\n**Reason**: ${reason || "Unspecified."}`
+        )
+        .catch(() => null);
+      return user
+        .ban({ reason: `${reason || "Unspecified"}` })
+        .then((_member) =>
+          message.channel.send({
+            content: `Successfully banned **${_member.user.tag}**`,
+          })
+        )
+        .catch((err) =>
+          message.channel.send({ content: `Failed to ban **${user.user.tag}` })
+        );
+    }}catch(err){
       
-  let data = await Guild.findOneAndUpdate({guildID: message.guild.id})
-  
-     let user = await message.mentions.members.first() || await message.guild.members.fetch(args[1])
-     
-      let reason = args.slice(2).join(" ");
-      const member = await message.guild.members.fetch(user.id).catch(() => {});
-if(member){
-    
-  
-    const memberPosition = member.roles.highest.position;
-			const moderationPosition = message.member.roles.highest.position;
-			if(message.member.ownerId !== message.author.id && !(moderationPosition > memberPosition)){
-				return message.channel.send({content:`You can't sanction or update a sanction for a member who has an higher or equal role hierarchy to yours!
-    `})
-			}
-			if(!member.bannable) {
-				return message.channel.send({content:`An error has occurred... Please check that I have the permission to ban this specific member and try again!`})
-			}
-		
-      await user.send(`**${message.author.tag}** banned you from ${message.guild.name}!\n**Reason**: ${reason|| 'Unspecified.'}`)
-    .catch(() => null);
-   return  user.ban({ reason: `${reason || 'Unspecified'}`})
-    .then(_member => message.channel.send({content:`Successfully banned **${_member.user.tag}**`}))
-    .catch((err) => message.channel.send({content:`Failed to ban **${user.user.tag}`}))
-  
-
-
-    
-    
-    }}
-}
+      return message.channel.send({content:` I can't ban this member check my permission or something `})
+    }
+  }
+};
